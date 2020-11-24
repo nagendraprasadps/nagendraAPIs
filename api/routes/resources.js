15f5db91cb64ee21010466d14734ed154c2bb63e
nagendra prasad <nagendra.prasad@medhandnet>
2020-11-24 13:43:13 +0530

    changed something

--- api/routes/resources.js
+++ api/routes/resources.js
@@ -12,16 +12,26 @@ const pool = new Pool({
 router.get('/:vfile', (req,res,next)=>{
     console.log("REACHED");
     vfile="./api/routes/" + req.url;
-    fs.readFile(vfile, function (err, data) {
-       
-        if (err) {
-            throw err;
-        }
-        movie_mp4 = data;
-        res.writeHead(200, {'Content-Type': 'video/mp4'});
-        res.write(data);
-        return res.end();
-    });
+    const range=req.headers.range;
+	if (!range){
+		res.status(400).send("Requires range header");
+    }
+    const videoPath=vfile;
+	const videoSize= fs.statSync(vfile).size;
+	const CHUNK_SIZE=10 ** 6;
+	const start=Number(range.replace(/\D/g,""));
+	const end= Math.min(start+ CHUNK_SIZE,videoSize-1);
+	const contentLength = end-start+1;
+	const headers={
+		"Content-Range": `bytes ${start}-${end}/${videoSize}`,
+		"Accept-Ranges"	: "bytes",
+		"Content-Length": contentLength,
+		"Content-Type": "video/mp4",
+	};
+	res.writeHead(206,headers);
+	const videoStream = fs.createReadStream(videoPath,{start,end});
+	
+	videoStream.pipe(res);
         
 });
    
