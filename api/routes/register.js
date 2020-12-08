517cbd4fc2dba48ba017abea4fbe3c6e91a10d96
nagendra prasad <nagendra.prasad@medhandnet>
2020-12-08 20:15:19 +0530

    changed something

--- api/routes/register.js
+++ api/routes/register.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 const express = require('express');
 const router=express.Router();
 const { Pool, Client } = require('pg');
-const connectionString = 'postgres://trikfpbvsuufym:26299662100d6bb1ac6e13dfbcecf3ecca09e85b6b59fab0b97c3a3bac15fde7@ec2-34-237-166-54.compute-1.amazonaws.com:5432/d1p47bpqtgcj4d'
+const connectionString = 'postgres://wxfmawymzebjgl:89ae8229f0b36d4e45fdead08bd8cfcf09b1740c61436388e1b9668e4c0fd4c7@ec2-18-233-207-22.compute-1.amazonaws.com:5432/dcs86ucjrgqgdf'
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 const pool = new Pool({
     connectionString: connectionString,
